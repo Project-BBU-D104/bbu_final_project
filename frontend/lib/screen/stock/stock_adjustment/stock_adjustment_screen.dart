e311fb4d426be3a5fba57c9f6d8926fad00c9d40
Sabrey1 <limsabriey94@gmail.com>
2026-06-01 16:44:11 +0700

    update

---
+++ frontend/lib/screen/stock/stock_adjustment/stock_adjustment_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StockAdjustmentScreen extends StatelessWidget {
+  const StockAdjustmentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Stock Adjustment Screen")),
+    );
+  }
+}
