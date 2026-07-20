2d9caf19780a6a4968cd97ba89495af70996ca6a
Sabrey1 <limsabriey94@gmail.com>
2026-07-20 12:17:59 +0700

    update

--- frontend/lib/screen/unit/unit_screen.dart
+++ frontend/lib/screen/unit/unit_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/screen/unit/widget/unit_card_widget.dart';
 import 'package:frontend/widget/custom_app_bar.dart';
+import 'package:frontend/widget/search_widget.dart';
 
 class UnitScreen extends StatelessWidget {
   const UnitScreen({super.key});
@@ -8,7 +10,24 @@ class UnitScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: "Unit"),
-      body: Text("Unit Screen"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SearchWidget(title: "Search Role"),
+          
+              UnitCardWidget(),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          // ctr.addRole(context);
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
---
+++ frontend/lib/screen/unit/widget/unit_card_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UnitCardWidget extends StatelessWidget {
+  const UnitCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Text("Unit Card Widget"),
+    );
+  }
+}
